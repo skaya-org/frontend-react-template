53038691376881ade89b504a8bfecaf52afe5842
shubhamkunwar10 <fonders@metakul.com>
2025-07-29 19:45:46 +0530

    use tsx

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '400px' }}>
+      <h1>Home Page</h1>
+      <p>This is the Home page</p>
+    </div>
+  );
+}
+
