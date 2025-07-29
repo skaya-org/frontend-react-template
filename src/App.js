2ac607e8085be5eccbc8774fef5b7cfdeb9a123a
shubhamkunwar10 <fonders@metakul.com>
2025-07-29 19:20:37 +0530

    Update build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <div className="App">
+    <div >
      <Homepage/>
     </div>
   );
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,7 +1,7 @@
 
 export default function Homepage() {
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '400px' }}>
       <h1>Profile Page</h1>
       <p>This is the user profile page</p>
     </div>
