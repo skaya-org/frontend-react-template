41f3461ad155fc0acc9db51c9bcd9958fda699c5
Shubham kunwar <founders@metakul.com>
2025-09-15 16:41:31 +0530

    Update craco.config.js with devServer settings
    Added devServer configuration to allow all hosts.

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,17 @@
-import CracoAlias from 'craco-alias';
+import CracoAlias from "craco-alias";
 
 export default {
   plugins: [
     {
       plugin: CracoAlias,
       options: {
-        source: 'tsconfig',
-        baseUrl: './',
-        tsConfigPath: './tsconfig.json'
-      }
-    }
-  ]
+        source: "tsconfig",
+        baseUrl: "./",
+        tsConfigPath: "./tsconfig.json",
+      },
+    },
+  ],
+  devServer: {
+    allowedHosts: "all" // ✅ allow all subdomains like *.skaya.org
+  },
 };
